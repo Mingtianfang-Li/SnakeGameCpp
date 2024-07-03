3f56d95efdef52a656ff33ccc4b002b440efed53
Ree Li <li.mingt@northeastern.edu>
2024-07-02 22:58:54 -0700

    Init Window

--- main.cpp
+++ main.cpp
@@ -3,5 +3,18 @@
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
+    std::string name = "Snake Game";
+    const char* title = name.c_str();
+    InitWindow(750, 750, title);
+    while (!WindowShouldClose()) {
+        // Update and draw
+        BeginDrawing();
+        ClearBackground(LIME);
+        DrawText("Snake Game", 500, 725, 20, LIGHTGRAY);
+        EndDrawing();
+    }
+
+    // De-Initialization
+    CloseWindow();
     return 0;
 }
